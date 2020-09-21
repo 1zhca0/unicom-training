9c46fa8e22aa1b150534a4fb380d5e6b6ec73ef8
1zca0 <yizheng1991@hotmail.com>
2020-09-21 14:24:41 +0800

    Initial commit

---
+++ ali-sonatype-nexus-staging/src/main/java/pro/yizheng/MainApplication.java
@@ -0,0 +1,11 @@
+package pro.yizheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
